89e1f9f64c1c9a679767312cf0532a6a078e6c6c
Faisal <faisal-f92@hotmail.com>
2020-02-06 01:00:56 +0300

    Custom Views - UIButton & UITextField

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GHFollowers
 //
 //  Created by Faisal Babkoor on 2/5/20.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .systemPink
     }
+    
 
 
 }
-
